1b3c103d3e2e55979fe68c20a61d73da8c17aea8
ZharasSembayev <jaks.sembaev07@gmail.com>
2026-03-23 17:00:02 +0500

    EightsLab

---
+++ Lab8/shop_back/api/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Product, Category
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Django with Zharas!")
+
+def products_list(request):
+    products = Product.objects.all()
+    data = []
+
+    for p in products:
+        data.append({
+            'id': p.id,
+            'name': p.name,
+            'price': p.price,
+            'description': p.description,
+            'count': p.count,
+            'is_active': p.is_active,
+            'category_id': p.category.id
+        })
+    return JsonResponse(data, safe=False)
+
+def product_detail(request, id):
+    try:
+        p = Product.objects.get(id=id)
+        data = {
+            'id': p.id,
+            'name': p.name,
+            'price': p.price,
+            'description': p.description,
+            'count': p.count,
+            'is_active': p.is_active,
+            'category_id': p.category.id
+        }
+        return JsonResponse(data)
+    except Product.DoesNotExist:
+        return JsonResponse({'error' : 'Not Found'})
+    
+def categories_list(request):
+    categories = Category.objects.all()
+    data = []
+
+    for c in categories:
+        data.append({
+            'id':c.id,
+            'name':c.name
+        })
+    return JsonResponse(data, safe=False)
+
+def category_detail(request, id):
+    try:
+        c = Category.objects.get(id=id)
+        data = {
+            'id':c.id,
+            'name':c.name
+        }
+        return JsonResponse(data)
+    except Category.DoesNotExist:
+        return JsonResponse({'error' : 'Not Found'})
+
+def category_products(request, id):
+    products = Product.objects.filter(category_id = id)
+    data = []
+
+    for p in products:
+        data.append({
+            'id': p.id,
+            'name': p.name,
+            'price': p.price
+        })
+    return JsonResponse(data, safe=False)
